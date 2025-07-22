f6aebb0eff1de9be3f518d42500e4114810e48f3
Code With Muaaz <codewithmuaaz@gmail.com>
2025-07-22 16:51:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const text = "Code With Muaaz";
 const heading = document.getElementById("typeHeading");
+let typingInterval;
 
 function startTyping() {
   let i = 0;
   heading.innerHTML = "";
 
-  const interval = setInterval(() => {
+  clearInterval(typingInterval); // Clear previous interval
+
+  typingInterval = setInterval(() => {
     heading.innerHTML += text.charAt(i);
     i++;
     if (i >= text.length) {
-      clearInterval(interval);
+      clearInterval(typingInterval);
     }
   }, 100);
 }
 
-
 startTyping();
-
-
-setInterval(startTyping, 4000);
+setInterval(startTyping, 4000); // Safe to repeat now
